be205a3f8151393fc188b1346ec348faf2bb6f4a
iquelee <iquelee@gmail.com>
2022-03-29 17:37:57 +0800

    master

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Input } from 'element-ui'
 
+Vue.prototype.$ELEMENT = { size: 'default', zIndex: 3000 }
+Vue.use(Input)
 Vue.config.productionTip = false
 
 new Vue({
